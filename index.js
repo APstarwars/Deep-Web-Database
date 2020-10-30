5f2d110d8e3651333aa588d5b9462c1425f921e6
APstarwars <hobbes399@gmail.com>
2020-10-29 20:03:05 -0700

    Add MySQL and Express, Start Server

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Hello World!");
+const mysql = require('mysql');
+const express = require('express');
+const fs = require('fs');
 
+const app = express();
+const port = 8080;
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "mvonblan_cs355fl20",
+	password: "vo7615745"
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.redirect('/index.html');
+});
+
+app.listen(port, () => {console.log(`Listening on http://blue.cs.sonoma.edu:${port}/`)});
